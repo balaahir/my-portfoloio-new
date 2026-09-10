5c20b157fac97e0693f664d300f68a36b3c400c7
Balaji <143376252+balaahir@users.noreply.github.com>
2026-09-11 01:34:14 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://kospzhwvquowrfzllksy.supabase.co';
+const supabaseAnonKey ='sb_publishable_VKe7IRBAnwHKm9Qmbvm-pA_BbWR5pgk';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
